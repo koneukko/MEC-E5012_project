893391217e67c382ed3b6fc1c39fd78308173190
Hörkkö Riku <horkkor1@vdiubuntu178.org.aalto.fi>
2024-10-07 16:19:34 +0300

    World corrdinate system implemented

--- vmc_project/src/racer.py
+++ vmc_project/src/racer.py
@@ -22,29 +22,55 @@ class Driving(): # main class
         
 
         self.distance = 0.19
-        if msg.ranges[0] > self.distance and msg.ranges[90] > self.distance and msg.ranges[270] > self.distance:
-        # clear path
-            trackdrive.linear.x = 0.4 # Forward through the track
-            trackdrive.angular.z = 0 # No rotation
-        if msg.ranges[275] < self.distance:
-        # path blocked by track wall at 275 deg
-            trackdrive.linear.x = 0.35
-            trackdrive.angular.z = 1.2 # Rotation to the left (ccw)
-            if msg.ranges [0] > self.distance and msg.ranges[30] > self.distance and msg.ranges[330] > self.distance:
-                # path is clear again
-                trackdrive.linear.x = 0.4
-                trackdrive.angular.z = 0 # No rotation
-        if msg.ranges[85] < self.distance:
-        #path blocked by track wall at 85 deg
-            trackdrive.linear.x = 0.35
-            trackdrive.angular.z = -1.2 # Rotation to the right (cw)
-            if msg.ranges [0] > self.distance and msg.ranges[30] > self.distance and msg.ranges[330] > self.distance:
-                # path is clear again
-                trackdrive.linear.x = 0.4
-                trackdrive.angular.z = 0 # No rotation
+        #All of the below is left as comments for now
+#        if msg.ranges[0] > self.distance and msg.ranges[90] > self.distance and msg.ranges[270] > self.distance:
+#        # clear path
+#            trackdrive.linear.x = 0.4 # Forward through the track
+#            trackdrive.angular.z = 0 # No rotation
+#        if msg.ranges[275] < self.distance:
+#        # path blocked by track wall at 275 deg
+#            trackdrive.linear.x = 0.35
+#            trackdrive.angular.z = 1.2 # Rotation to the left (ccw)
+#            if msg.ranges [0] > self.distance and msg.ranges[30] > self.distance and msg.ranges[330] > self.distance:
+#                # path is clear again
+#                trackdrive.linear.x = 0.4
+#                trackdrive.angular.z = 0 # No rotation
+#        if msg.ranges[85] < self.distance:
+#        #path blocked by track wall at 85 deg
+#            trackdrive.linear.x = 0.35
+#            trackdrive.angular.z = -1.2 # Rotation to the right (cw)
+#            if msg.ranges [0] > self.distance and msg.ranges[30] > self.distance and msg.ranges[330] > self.distance:
+#                # path is clear again
+#                trackdrive.linear.x = 0.4
+#                trackdrive.angular.z = 0 # No rotation
+
+        #initalize the distance
+        print(type(msg.ranges))
+        Gamma=np.argmin(msg.ranges)+180
+        print("Smallest distance index (DEG):",Gamma)
+        theta=Gamma-90
+
+        #Smallest distandce to the wall and it's opposite
+        Least = msg.ranges[np.argmin(msg.ranges)]
+        Opposite = msg.ranges[Gamma]
+
+        #Initalize vehicle coordinate system orgin in world coordinate system accordding to palnned drawings
+        y_vehicle = (Least + Opposite)/2 - Least
+        x_vehicle = 0 # The world coordinate system is at this place
+
+        #Lookahead point in world coordinate system
+        x_g=0.5
+        y_g=0
+
+        #coordinate transformation
+        x_gv = (x_g - x_vehicle)*np.cos(np.deg2rad(theta)) + (y_g - y_vehicle)*np.sin(np.deg2rad(theta))
+        y_gv = (x_g - x_vehicle)*np.sin(np.deg2rad(theta)) + (y_g - y_vehicle)*np.cos(np.deg2rad(theta))
+
+
         if msg.ranges[90] == float("inf") and msg.ranges[270] == float("inf"):
             trackdrive.linear.x = 0 # End of track
             trackdrive.angular.z = 0 # End of track
+
         self.pub.publish(trackdrive) # Publish command for forward movement
 
 
